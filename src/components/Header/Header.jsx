60e08a4d7c2593e9e805068118c3fce64e3be82f
elvirahafis19 <hacacadua@gmail.com>
2025-03-12 10:52:04 +0700

    first

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-contents">
+        <h2>Good Food Us <p><h2>Good Mood</h2></p> </h2>
+        <p>
+          I would think that conserving our natural Resources
+          Should be conservative position: Not to waste food, and 
+          not to throw away a lot of the food that we
+        </p>
+        <a href="#explore-menu">
+          <button >Daftar Sekarang</button>
+          <button  style={{backgroundColor: "#FFFFFF"}}>About Us</button>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ setShowLogin }) => {
+  const { getTotalQuantity } = useContext(StoreContext);
+  const totalQuantity = getTotalQuantity();
+
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("About")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          About
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Promotions")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Promotions
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Blogs")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Blogs
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <a
+            href="#footer"
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            Masuk
+          </a>
+        <button className="button-right" onClick={() => setShowLogin(true)}>Daftar Sekarang</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
